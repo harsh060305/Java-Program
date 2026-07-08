c95eef8ea8358d0969a8e8e59514f21e4d1fa794
Harsh Maurya <harsh060305@gmail.com>
2026-07-08 15:18:50 +0530

    Update Array_sorted.java

--- Array_sorted.java
+++ Array_sorted.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
         
             System.out.println("enter the element of array");  
             int[] arr=new int[n];
-            
+             
             for(int i=0;i<n;i++){
                 arr[i]=sc.nextInt();
             }
